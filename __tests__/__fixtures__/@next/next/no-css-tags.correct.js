bfbab7b0a6b5e97bc02be5f932efc84bc08b08f5
Bertrand Marron <bertrand.marron@gmail.com>
2021-04-14 14:27:59 +0200

    Update @next/next/no-css-tags test

--- __tests__/__fixtures__/@next/next/no-css-tags.correct.js
+++ __tests__/__fixtures__/@next/next/no-css-tags.correct.js
@@ -1,13 +1,15 @@
 export default function Page() {
+  const path = '/style.css'
+
   return (
     <div>
       <link
-        rel='foo' // The rule only checks for rel='stylesheet' :)
+        rel='foo' // The rule only checks for rel='stylesheet'
         href='/style.css'
       />
       <link
         rel='stylesheet'
-        href='https://example.org/style.css' // Loading external links is OK.
+        href={path} // The rule only checks Literals
       />
     </div>
   )
