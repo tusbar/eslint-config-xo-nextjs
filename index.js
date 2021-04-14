996a455e27ea28e7a552cb45ca73e21e09a6b3a5
Bertrand Marron <bertrand.marron@gmail.com>
2021-04-14 14:22:50 +0200

    Update code formatting

--- index.js
+++ index.js
@@ -32,47 +32,71 @@ module.exports = {
     ],
 
     // JSX
-    'jsx-quotes': ['error', 'prefer-single'],
+    'jsx-quotes': [
+      'error',
+      'prefer-single'
+    ],
 
     // React
-    'react/boolean-prop-naming': ['warn', {
-      validateNested: true
-    }],
+    'react/boolean-prop-naming': [
+      'warn',
+      {
+        validateNested: true
+      }
+    ],
     'react/forbid-component-props': 'off',
-    'react/function-component-definition': ['error', {
-      namedComponents: 'function-declaration',
-      unnamedComponents: 'arrow-function'
-    }],
-    'react/jsx-closing-bracket-location': ['error', {
-      nonEmpty: 'tag-aligned',
-      selfClosing: false
-    }],
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: 'function-declaration',
+        unnamedComponents: 'arrow-function'
+      }
+    ],
+    'react/jsx-closing-bracket-location': [
+      'error',
+      {
+        nonEmpty: 'tag-aligned',
+        selfClosing: false
+      }
+    ],
     'react/jsx-closing-tag-location': 'off',
-    'react/jsx-curly-brace-presence': ['error', {
-      props: 'never',
-      // This allows <style jsx>{`…`}</style>
-      children: 'ignore'
-    }],
+    'react/jsx-curly-brace-presence': [
+      'error',
+      {
+        props: 'never',
+        // This allows <style jsx>{`…`}</style>
+        children: 'ignore'
+      }
+    ],
     'react/jsx-first-prop-new-line': 'off',
     'react/jsx-handler-names': 'off',
     'react/jsx-max-props-per-line': 'off',
     'react/jsx-one-expression-per-line': 'off',
-    'react/jsx-tag-spacing': ['error', {
-      closingSlash: 'never',
-      beforeSelfClosing: 'always',
-      afterOpening: 'never'
-    }],
-    'react/jsx-no-duplicate-props': ['error', {
-      // This allows material-ui’s usage of inputProps and InputProps together:
-      // <TextField inputProps={{…}} InputProps={{…}} /> errors otherwise.
-      ignoreCase: false
-    }],
+    'react/jsx-tag-spacing': [
+      'error',
+      {
+        closingSlash: 'never',
+        beforeSelfClosing: 'always',
+        afterOpening: 'never'
+      }
+    ],
+    'react/jsx-no-duplicate-props': [
+      'error',
+      {
+        // This allows material-ui’s usage of inputProps and InputProps together:
+        // <TextField inputProps={{…}} InputProps={{…}} /> errors otherwise.
+        ignoreCase: false
+      }
+    ],
     'react/jsx-uses-react': 'off',
     'react/react-in-jsx-scope': 'off',
-    'react/require-default-props': ['warn', {
-      forbidDefaultForRequired: true,
-      ignoreFunctionalComponents: true
-    }],
+    'react/require-default-props': [
+      'warn',
+      {
+        forbidDefaultForRequired: true,
+        ignoreFunctionalComponents: true
+      }
+    ],
 
     // Next.js
     '@next/next/no-css-tags': 'error',
